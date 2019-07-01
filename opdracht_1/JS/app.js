a265fd638aed50bfede4a6934b32f026982788f4
Martijn <127121@student.drenthecollege.nl>
2019-07-01 09:01:56 +0200

    Opdracht 2 afgerond.

--- opdracht_1/JS/app.js
+++ opdracht_1/JS/app.js
@@ -1,1 +1,9 @@
 /* Schrijf javascript hier */
+let firstName = "Henk";
+let lastName = "Pietersma";
+
+document.getElementById('innerHTML').innerHTML = `${firstName} ${lastName}`;
+
+document.write(`${firstName} ${lastName}`);
+
+window.alert(`${firstName} ${lastName}`);
