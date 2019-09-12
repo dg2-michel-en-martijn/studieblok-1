aa3821d0a25ad5049ddba0255920547d2ede8d82
Martijn <127121@student.drenthecollege.nl>
2019-09-12 10:49:01 +0200

    Comments toegevoegd en laatste calculaties

--- opdracht_2/JS/app.js
+++ opdracht_2/JS/app.js
@@ -3,9 +3,12 @@ var a = 3;
 var b = 4;
 var c = 5;
 
-document.write(a + b + c);
+document.write(a + b + c);      //3 + 4 + 5 = 12
 document.write("<br>")
-document.write(a + c - b);
+document.write(a + c - b);      // 3 + 5 - 4 = 4
+document.write("<br>");
+document.write(c % a);          // 5 / 3 = 1 rest 2
 document.write("<br>")
-document.write(c % a);
-document.write("<br>")
+document.write((a - c) * b);    // (3 - 5) * 4 = -8
+document.write("<br>");
+document.write((b + c) / a );   // (4 - 5) * 3 = 3
